175865d5bc5162f9258634c3a1c178fb564ffb51
dawoori <daul@kookmin.ac.kr>
2020-10-13 21:03:32 +0900

    version 2

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,6 +1,6 @@
 object HelloWorld {
   def main(args: Array[String]) {
-    println("Hello, world!")
+    println("Hello, world 2!")
     val list = List(1, -2, -3, 4).map(x => Math.abs(x))
     val list2 = List(1, 2, 3, 4).reduce((x,y) => x*y)
     println(list)
