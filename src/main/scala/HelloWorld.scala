7d6dcd603d8d2a9557f821eb3e002fdffed0380b
dawoori <daul@kookmin.ac.kr>
2020-10-13 14:17:49 +0900

    initial commit.

---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,9 @@
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Hello, world!")
+    val list = List(1, -2, -3, 4).map(x => Math.abs(x))
+    val list2 = List(1, 2, 3, 4).reduce((x,y) => x*y)
+    println(list)
+    println(list2)
+  }
+}
